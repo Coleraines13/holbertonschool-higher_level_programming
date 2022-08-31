387eef8443b2b3e411aeb8df74718db2c5e246a0
root <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>
2022-08-31 15:20:30 -0700

    probs fixed now

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if length == 1:
         print("{:d} arguments.".format(length - 1))
     elif length == 2:
-        print("{:d} arguments:".format(length - 1))
+        print("{:d} argument:".format(length - 1))
     else:
         print("{:d} arguments:".format(length - 1))
     for i in range(1, length):
