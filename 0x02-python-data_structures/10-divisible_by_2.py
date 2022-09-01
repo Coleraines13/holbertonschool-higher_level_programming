7518a78763e365bf3770bb69c975ee7ddf36160f
root <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>
2022-09-01 16:11:42 -0700

    fixed 10

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,13 @@
 
 
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-        return new_list
+    if my_list is None:
+        return(my_list)
+    else:
+        values = []
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                values.append(True)
+            else:
+                values.append(False)
+            return(values)
