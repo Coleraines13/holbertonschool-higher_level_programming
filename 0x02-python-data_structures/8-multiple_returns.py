a42d831f04dcaeb4ba616fd77ecde142aa889103
root <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>
2022-09-01 15:58:12 -0700

    added 8

---
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if not sentence:
+        firchar = None
+    else:
+        firchar = sentence[0]
+        longi= len(sentence)
+        newtuple = longi, firchar
+        return(newtupla)
