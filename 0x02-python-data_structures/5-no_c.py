67a5a3386a98bb9d488d6dfe7358288ce1ad9d8e
root <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>
2022-09-01 15:55:02 -0700

    came back to 5

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            str = str + i
-        return str
+    if my_string is not None:
+        new = my_string.translate({ord('C'): ""})
+        new2 = new.translate({ord('c'): ""})
+        return (new2)
