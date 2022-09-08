dfaa5d3d1b4d6606aaaa9eead7b6574be3c31dc6
root <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>
2022-09-07 19:13:34 -0700

    ahhh

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
-
 """raising exceptions for square"""
-class Square:
+
+
+class Square:   
     """created class Square"""
     def __init__(self, size=0):
         """added attributes to square"""
