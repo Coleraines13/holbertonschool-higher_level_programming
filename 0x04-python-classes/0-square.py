bbc79867f0a8a90e3e8d73fa824d7ee03f76c470
root <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>
2022-09-07 18:59:31 -0700

    fixed 0

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -3,6 +3,11 @@
 module 0-square
 Defines class Square
 """
+
+
+
 class Square:
-    """empty class"""
-    pass
+    """created square class"""
+    def __init__(self, size):
+        """initilizing attributes"""
+        self.__size = size
