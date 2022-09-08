5602aa65bbcce4033e84a0767063c6dc08b6e84d
root <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>
2022-09-07 19:20:56 -0700

    working on it

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""added method area"""
 
 
 class Square:
+    """created class square"""
     def __init__(self, size=0):
+        """assign atrributes to square"""
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
