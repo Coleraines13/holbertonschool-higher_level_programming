2e496d49a932da29409fbefbc1ad5007661980fd
root <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>
2022-09-07 19:36:34 -0700

    fixed 4

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -2,27 +2,36 @@
 """added method area"""
 
 
-
 class Square:
     """created class square"""
     def __init__(self, size=0):
         """assign atrributes to square"""
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
-    # Property
+       def area(self):
+           return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
 
-    # Setter modifies
     @size.setter
     def size(self, value):
+        self.__size = value
         if type(value) != int:
-            raise TypeError('size must be an integer')
+            raise TypeError("size must be integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
 
-    def area(self):
-        return self.__size ** 2
+    def my_print(self):
+        if self.__size <= 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end='')
+                print()
