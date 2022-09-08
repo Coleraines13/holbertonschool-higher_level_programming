31eba8cc7c07b685d6b7a4c31829eeaeebe89027
root <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>
2022-09-07 19:25:55 -0700

    fixed 3

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -10,6 +10,18 @@ class Square:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
-        else:
-            self.__self.__size
-            return size * size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
