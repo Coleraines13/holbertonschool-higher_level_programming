c07221fceefe502e83aa16b23bb0b1edc895bc72
root <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>
2022-09-11 10:10:29 -0700

    fixing 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
 """ Temp """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
+
     def test_max_int_basic(self):
+
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
     def test_max_int_empty(self):
+
         self.assertEqual(max_integer([]), None)
+
     def test_max_int_neg(self):
+
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        
     def test_max_int_one(self):
+
         self.assertEqual(max_integer([1]), 1)
 
     if __name__ == '__main__':
