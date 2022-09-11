171722df6d5a1e1b98703afb7f57d115eb48996c
root <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>
2022-09-11 09:08:41 -0700

    attemped to get all check 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,9 +2,9 @@
 """ Temp """
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must ne an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be integer")
+        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
