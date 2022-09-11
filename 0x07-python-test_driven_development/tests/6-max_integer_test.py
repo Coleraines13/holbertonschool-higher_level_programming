8889f22b2fa959ffded898b7624349f53ea8f84d
root <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>
2022-09-11 10:08:37 -0700

    added 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Temp """
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_int_basic(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_max_int_empty(self):
+        self.assertEqual(max_integer([]), None)
+    def test_max_int_neg(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    def test_max_int_one(self):
+        self.assertEqual(max_integer([1]), 1)
+
+    if __name__ == '__main__':
+        unittest.main()
