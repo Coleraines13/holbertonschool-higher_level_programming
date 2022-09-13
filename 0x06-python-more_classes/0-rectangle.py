c03a99b197ef4fb3daca38783dfaa6d9942e1c48
root <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>
2022-09-13 11:04:27 -0700

    fixing 0 and 1

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -29,13 +29,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = value    
-    
+            self.__width = value
     @property
     def height(self):
 
         return self.__height
-    
     @height.setter
     def height(self, value):
 
