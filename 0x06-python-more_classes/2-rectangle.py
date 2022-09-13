162c1269e5b00cdbb97554a1adfa842e10b8482f
root <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>
2022-09-13 13:33:12 -0700

    fixed 2

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -35,26 +35,26 @@ class Rectangle:
         str = ("{}\n".format("#" * self.__width) * self.__height)
         return str[:-1]
 
-     @property
-     def width(self):
-         return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-     @property
-     def height(self):
-         return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-     @width.setter
-     def width(self, value):
-         if type(value) != int:
-             raise TypeError("width must be an integer")
-         elif value < 0:
-             raise ValueError("width must be >= 0")
-         self.__width = value
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-     @height.setter
-     def height(self, value):
-          if type(value) != int:
-              raise TypeError("height must be an integer")
-          elif value < 0:
-              raise ValueError("height must be >= 0")
-          self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
