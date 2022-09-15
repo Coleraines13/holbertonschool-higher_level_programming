8e1331976baee1188a259d497940c2a3c2f99b4f
root <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>
2022-09-15 14:10:17 -0700

    fixed 1 mabye

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" Mylist"""
+"""Mylist"""
 
 
 class Mylist(list):
     """ contains list"""
     def print_sorted(self):
+        """prints sorted list"""
         print(sorted(self))
