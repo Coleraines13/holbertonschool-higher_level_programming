3b311b060a599f4d9821b7db1892bb0766a45680
root <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>
2022-09-19 18:26:24 -0700

    added 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""input/output"""
+
+
+Student = __import__('10-student').Student
+def reload_from_json(self, json):
+    self,json = json
+    for i in json:
+        setattr(self, i, json[i])
