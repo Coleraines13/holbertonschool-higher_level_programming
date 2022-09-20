39a8b57dd0f5a45ad283658cb14d5fe60705f99e
root <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>
2022-09-19 18:21:11 -0700

    added 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""input/output"""
+
+
+class Student:
+    """make a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        self.attrs = attrs
+        attr_list = {}
+        if attrs is None:
+            return self.__dict__
+        for i in attrs:
+            if i in self.__dict__:
+                attr_list[i] = self.__dict__[i]
+        return attr_list
