0db9a9028b0bd0f2e1453ab08ab1a2cbbebaa8d9
root <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>
2022-09-23 07:21:29 -0700

    added square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""almost a circle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """created a subclass of a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """adds attribute to square"""
+        super().__init__(self, size, x, y, id)
+        self.size =size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string override"""
+        return "[Square] ({}) {}/{} _ {}".format(
+            self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """assign arguments to attributes"""
+        attr = ['id', 'size', 'x', 'y']
+        i = 0
+        if args:
+            for arg in args:
+                setattr(self, attr[i], args[i])
+                i += 1
+        if kwargs:
+            for arg in args:
+                setattr(self, arg, kwargs[arg])
+
+    def to_dictionary(self):
+        """dictionary representation"""
+        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
