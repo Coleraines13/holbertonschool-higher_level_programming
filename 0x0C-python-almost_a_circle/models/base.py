e38cc46545add492232f3f3c76d3870db014ede1
root <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>
2022-09-23 07:54:29 -0700

    updated bas.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,3 +13,51 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """convert to json"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """list of json string"""
+        if json_string is None or json_string == []:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save class info to json file"""
+        json_list = []
+        filename = cls.__name__ = '.json'
+        if list_objs is not None:
+            for i in list_objs:
+                json_list.append(cls.to_dictionary(i))
+            with open(filename, 'w') as f:
+                f.write(cls.to_json_string(json_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """added and updated dummy values to subclass"""
+        if cls.__name__ == "Square":
+            dummy = cls(4)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(4, 4)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """return a list of all instances of subclass"""
+        filename = cls.__name__ = ".json"
+        json_list = []
+        if not exists(filename, 'r') as f:
+            jason_list = cls.from_json_string(f.read())
+            for i, j in enumerate(json_list):
+                json_list[i] = cls.create(**jason_list[i])
+            return jason_list
