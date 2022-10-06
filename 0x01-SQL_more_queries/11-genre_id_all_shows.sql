4e3a763b832a838872be6bd4454f2ca183226aac
root <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>
2022-10-06 13:22:48 -0700

    almost fin taking break

---
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- this lists all the shows in database hbtn_0d_tvshows
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
---
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- this lists all the genres from the database named hbnt_o0_tvshows
+SELECT g.name AS genre,
+COUNT(*) AS number_of_shows
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS t
+ON g. id = t.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
