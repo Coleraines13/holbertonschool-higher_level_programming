7334959a43639fce1b793e998c29206fdf71c653
root <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>
2022-10-19 12:44:58 -0700

    working on it

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """this lists all states with names that start with a uppercase n"""
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1],
                         passwd=sys.argv[2],
                         db=sys.argv[3],
                         host='localhost',
                         port=3306)
     cur = db.cursor()
-    cmd = """SELECT id, name
-             FROM states
-             WHERE name LIKE BINARY 'N%'
-             ORDER BY id ASC;"""
-    cur = db.cursor()
+    cmd = ("SELECT * FROM states ORDER BY id ASC")
+    cur.execute(cmd)
     nStates = cur.fetchall()
 
     for state in nStates:
