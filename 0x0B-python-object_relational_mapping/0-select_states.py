db44880985a2a65bcbabb7ca1ac93d0b67371f4f
root <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>
2022-10-19 12:02:49 -0700

    fixing it

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            host='localhost',
-            port=3306)
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        host='localhost',
+                        port=3306)
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     allStates = cur.fetchall()
