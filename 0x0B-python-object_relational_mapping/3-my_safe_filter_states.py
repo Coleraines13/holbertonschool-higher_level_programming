9a2e47518f75799a0683418e800cd3b2e276dbe1
root <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>
2022-10-19 13:21:39 -0700

    3 done

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""this takes in arguments and displays all values"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
+    cmd = ("SELECT * FROM states WHERE name=%s ORDER BY id ASC")
+    cur.execute(cmd, (sys.argv[4],))
+    nStates = cur.fetchall()
+
+    for state in nStates:
+        print(state)
+
+    cur.close()
+    db.close()
