584fe694db3620797b4c28cec40eca9fc3868951
root <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>
2022-10-20 05:33:55 -0700

    added 7 and fixed model.state

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""this lists all state objects in the database given"""
-import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from model_state import Base, State
+"""sqlalchemy"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Session = sessionmaker(bind=engine)
-    session = Session()
+Base = declarative_base
 
-    for instance in session.query(State):
-        print("{:d}: {}".format(instance.id, instance.name))
+
+class State(Base):
+    """this the the model foir state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True,
+            nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
