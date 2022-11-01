9bf59f81a992c8471c284a4fac61defe028d1175
root <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>
2022-11-01 14:48:19 -0700

    working on it

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("name")))
-            except ValueError:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
                 print("Not a valid JSON")
