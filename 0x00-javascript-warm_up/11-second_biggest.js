385a7fb1781dd140262a01e5788ae92682c46367
root <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>
2022-12-01 18:39:43 -0800

    fixed everythingt

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
 } else {
   const args = process.argv.map(Number)
   .slice(2, process.argv.length)
-  .sort((a, b)= > a - b)
+  .sort((a, b) => a - b)
   console.log(args[args.length - 2])
 }
