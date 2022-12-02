ef5ade01ff1b2e0fb2c7e77e75191967211e6979
root <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>
2022-12-02 12:00:15 -0800

    fixed

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
-
-class Square extends Rectangle {	
+module.exports = class Square extends require('./4-rectangle.js') {
   constructor(size) {
     super(size, size);
   }
